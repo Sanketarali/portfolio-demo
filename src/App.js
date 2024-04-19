1e8a67c4e365ddbf5017e4b4a341d0d14ddd866c
Sanket Arali <110754364+Sanketarali@users.noreply.github.com>
2024-04-19 12:52:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import React from 'react'
+import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
-import Trending from './pages/Trending';
-
+import Resume from './pages/resume'; // Corrected import with uppercase R
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={ <Home /> } />
-          <Route path='/contact' element={ <Contact /> } />
-          <Route path='/trending' element={ <Trending /> } />
+          <Route path='/' element={<Home />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/resume' element={<Resume />} /> {/* Corrected usage of Resume */}
         </Routes>
       </BrowserRouter>
     </>
