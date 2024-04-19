f762a7a48bca568ae6407ad5fde0e9158acc203c
Sanket Arali <110754364+Sanketarali@users.noreply.github.com>
2024-04-19 12:52:49 +0530

    Update resume.jsx

--- src/pages/Trending.jsx
+++ src/pages/Trending.jsx
@@ -1,37 +1,3 @@
-
-
-
-
-// import React from "react";
-// import Layout from "../components/Layout";
-
-// export default function Trending() {
-//   return (
-//     <Layout>
-//       <div className="w-[100%] h-[84vh] text-white text-3xl my-6 flex flex-col items-center justify-center">
-//         <iframe src="/assets/cover.jpeg" className="w-full h-full" title="Resume"></iframe>
-//         <a href="assets/resume.pdf" download className="mt-4 text-blue-500 block text-center">Download PDF</a>
-//       </div>
-//     </Layout>
-//   );
-// }
-
-
-// import React from "react";
-// import Layout from "../components/Layout";
-
-// export default function Trending() {
-//   return (
-//     <Layout>
-//       <div className="w-[100%] h-[84vh] text-white my-6 flex flex-col items-center justify-center">
-//         <iframe src="./assets/cover.jpeg" className="w-full h-full" title="Resume"></iframe>
-//         <a href="./assets/cover.jpeg" download className="mt-4 text-blue-500 block text-center">Download PDF</a>
-//       </div>
-//     </Layout>
-//   );
-// }
-
-
 import React from 'react';
 
 function App() {
@@ -40,7 +6,7 @@ function App() {
     console.log('Downloading...'); // Optional: Log a message
 
     // Simulate downloading a file
-    const url = 'https://drive.google.com/file/d/1C8pertni7jKo994aZcltIeOGgljcYW_G/view'; // Replace with your file URL
+    const url = 'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf'; // Replace with your file URL
     const link = document.createElement('a');
     link.href = url;
     link.download = 'sanketarali_resume.pdf'; // Specify the filename for download
@@ -57,4 +23,3 @@ function App() {
 }
 
 export default App;
-
